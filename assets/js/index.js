fd2cfcad3bac71f114037ab94011915ed3d32afd
João Rafael <jrfifipi@gmail.com>
2024-09-08 20:34:13 -0300

    projeto remodelado para aleatoriedade

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
+const baseUrl = 'https://dogapi.dog/api/v2/breeds?page[number]='
+
 function fazGet(url) { // faz a requisição do conteúdo do site (tipo GET[pegar informação])
     const request = new XMLHttpRequest();
     request.open("GET", url , false);
     request.send();
     
     return request.responseText;
+}
+
+function lerPaginaEPegarBanco (Pagina) {
+    const data =  fazGet(`${baseUrl}${Pagina}`);
+
+    // const data = await fetch(`${baseUrl}${Pagina}`);
+
+    var usuarios = JSON.parse(data).data ;
+    
+    
+    const limpesa = document.getElementById('tabela');
+    limpesa.innerHTML = ''; // Limpa o conteúdo anterior
+
+    return criaLinha(usuarios[numeroAtual]);
 }
 
 function criaLinha(usuarios) { // cria o conteúdo da tabela 
@@ -13,8 +29,17 @@ function criaLinha(usuarios) { // cria o conteúdo da tabela
     const tdDesc = document.createElement("td");// linhas do conteúdo
 
     // pego a informação pelo fazGet e com o caminho até a página
-    tdDesc.innerHTML = usuarios.attributes.description 
+
     tdName.innerHTML = usuarios.attributes.name;
+    tdDesc.innerHTML = usuarios.attributes.description ;
+
+    // tdName.innerHTML = `<td>${usuarios[numeroAtual].attributes.name} </td> ` ;
+    // tdDesc.innerHTML = `<td> ${usuarios[numeroAtual].attributes.description} </td> ` ;
+
+    // tdName.innerHTML = `${usuarios.attributes.name}` ;
+    // tdDesc.innerHTML = `${usuarios.attributes.description}` ;
+
+   
 
     linha.appendChild(tdName); // leva o conteúdo até o HTML
     linha.appendChild(tdDesc);
@@ -22,143 +47,38 @@ function criaLinha(usuarios) { // cria o conteúdo da tabela
     return linha; // trás toda a informação pra onde é requisitado...
 }
 
-
-function lerPaginaEPegarBanco (Pagina) {
-    const data = fazGet(`https://dogapi.dog/api/v2/breeds?page[number]=${Pagina}`);
-    var usuarios = JSON.parse(data).data;
-    
-    
-
-    const limpesa = document.getElementById('tabela');
-    limpesa.innerHTML = ''; // Limpa o conteúdo anterior
-
-    var botao = document.getElementById('dog-form3')
-
-    if (Pagina > 29) {
-        const limpesa = document.getElementById('tabela');
-        limpesa.innerHTML = ''; 
-        window.alert("Fim das páginas, resete ou volte algumas páginas")
-        botao.style.display = 'none'
-    }
-    else {
-        botao.style.display = 'block'
-    }
-
-
-    return criaLinha(usuarios[numeroAtual]);
-}
-
-
-
-
-var numeroPaginaAtual = 1
-var numeroAtual = 1
-
-async function main(event) {
+function main(event) {
     event.preventDefault(); 
 
-    numeroAtual -= 1
+    numeroAtual = Math.floor(Math.random() * 9 )
+    numeroPaginaAtual = Math.floor(Math.random() * 29 )
 
-    if (numeroAtual < 0) {
-        const limpesa = document.getElementById('tabela');
-        limpesa.innerHTML = ''; // Limpa o conteúdo anterior
-        numeroAtual = 0
-        window.alert("Acesso Invalido!")
+    if (numeroPaginaAtual == 29) {
+        console.log(numeroAtual , ' entrou na página!')
+        console.log(numeroPaginaAtual , ' entrou na página!')
+        numeroAtual = Math.floor(Math.random() * 7 )
     }
-    linha = lerPaginaEPegarBanco(numeroPaginaAtual)
+    
     
     console.log(numeroAtual)
-
-
-    const tabela = document.getElementById("tabela");
-    tabela.appendChild(linha);
-    
-}
-
-async function main2(event) {
-    event.preventDefault();  
-    
-    numeroAtual += 1
-    if (numeroAtual > 9) {
-        const limpesa = document.getElementById('tabela');
-        limpesa.innerHTML = ''; // Limpa o conteúdo anterior
-        window.alert("Número máximo de cachorros, avance para ver mais!")
-        numeroAtual = 9
-    }
-
-    linha = lerPaginaEPegarBanco(numeroPaginaAtual) 
-    
-    console.log(numeroAtual)
-
-    const tabela = document.getElementById("tabela")
-    tabela.appendChild(linha);
-}
-
-
-
-async function main3(event) {
-    event.preventDefault();  
-    numeroAtual = 0
-    numeroPaginaAtual += 1
-
-    linha = lerPaginaEPegarBanco(numeroPaginaAtual)
-    
     console.log(numeroPaginaAtual)
+
+    linha = lerPaginaEPegarBanco(numeroPaginaAtual , )
     
+    
+    
+    // console.log(typeof(tdDesc))
+    // console.log(typeof(tdName))
 
     const tabela = document.getElementById("tabela");
     tabela.appendChild(linha);
-
+    
 }
 
-async function main4(event) {
-    event.preventDefault();  
+const dogGenerate = document.getElementById('dog-random');
+dogGenerate.addEventListener('submit', main);
 
-    numeroPaginaAtual -=1
-    numeroAtual = 0
 
-    if (numeroPaginaAtual <= 0 ) {
-        window.alert("Não é possivel voltar")
-        numeroPaginaAtual = 1
-    }
 
-    linha = lerPaginaEPegarBanco(numeroPaginaAtual)
-    
-    console.log(numeroPaginaAtual)
-    
-   
-    const tabela = document.getElementById("tabela");
-    tabela.appendChild(linha);
-
-}
-
-async function main5(event) {
-    event.preventDefault();  
-
-    numeroPaginaAtual = 1
-    numeroAtual = 0
-
-    linha = lerPaginaEPegarBanco(numeroPaginaAtual)
-    
-    console.log(numeroPaginaAtual)
-    
-
-    const tabela = document.getElementById("tabela");
-    tabela.appendChild(linha);
-
-}
-
-const dogForm = document.getElementById('dog-form');
-dogForm.addEventListener('submit', main);
-
-const dogForm2 = document.getElementById('dog-form2');
-dogForm2.addEventListener('submit', main2);
-
-const dogForm3 = document.getElementById('dog-form3');
-dogForm3.addEventListener('submit', main3);
-
-const dogForm4 = document.getElementById('dog-form4');
-dogForm4.addEventListener('submit', main4);
-
-const dogForm5 = document.getElementById('dog-form5');
-dogForm5.addEventListener('submit', main5);
+// response = fazGet(url)
+// response.blob() --- converte o arquivo em binário, por exemplo, para uma imagem você converte o link para binário e depois
